1c8e00fc636478d46c9462b14ba3657404002883
laurenskz <laurenskz@gmail.com>
2014-11-25 22:20:39 +0100

    new things added

--- CreateMajorChords.java
+++ CreateMajorChords.java
@@ -5,15 +5,16 @@
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class CreateChordProgression
+public class CreateMajorChords
 {
     // instance variables - replace the example below with your own
-    private int x;
+    private int startChord;
+    private int lastChordCounter;
 
     /**
      * Constructor for objects of class CreateChordProgression
      */
-    public CreateChordProgression()
+    public CreateMajorChords()
     {
         // initialise instance variables
         x = 0;
